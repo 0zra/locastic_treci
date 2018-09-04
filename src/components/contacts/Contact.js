e763b7125996ba4a10b8fb6ffb5389557526654a
ozra <petar.ozretic@mail.com>
2018-09-04 09:18:36 +0200

    Add Remove-Contact button functionality

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-
+import { Consumer } from "../../context";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  onDeleteClick = async (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
+  };
   render() {
     const {
       name,
@@ -17,26 +20,45 @@ class Contact extends Component {
     } = this.props.contact;
 
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name} {surname}{" "}
-          <i
-            onClick={() => alert("Ovo ce bit link za prikaz cilog kontakta")}
-            className="fas fa-plus"
-            style={{ cursor: "pointer", float: "right", color: "blue" }}
-          />
-        </h4>
-        {/*
-        <ul className="list-group">
-          <li className="list-group-item">Adress:{adress}</li>
-          <li className="list-group-item">Postal code:{postal_code}</li>
-          <li className="list-group-item">City:{city}</li>
-          <li className="list-group-item">Country:{country}</li>
-          <li className="list-group-item">Phone:{phone}</li>
-          <li className="list-group-item">Email:{email}</li>
-        </ul>
-        */}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name} {surname}{" "}
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+                <i
+                  onClick={() =>
+                    alert("Ovo ce bit link za prikaz cilog kontakta")
+                  }
+                  className="fas fa-plus"
+                  style={{
+                    cursor: "pointer",
+                    float: "right",
+                    color: "blue",
+                    marginRight: "1rem"
+                  }}
+                />
+              </h4>
+              {/*
+              <ul className="list-group">
+                <li className="list-group-item">Adress:{adress}</li>
+                <li className="list-group-item">Postal code:{postal_code}</li>
+                <li className="list-group-item">City:{city}</li>
+                <li className="list-group-item">Country:{country}</li>
+                <li className="list-group-item">Phone:{phone}</li>
+                <li className="list-group-item">Email:{email}</li>
+              </ul>
+              */}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
     contacts: [
@@ -27,7 +42,10 @@ export class Provider extends Component {
         phone: "460-555",
         email: "bla@blabla.com"
       }
-    ]
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
   };
 
   render() {
